d9c2a2ea92789528dc1fa7cae046f1aec3c0d518
Aurélien Grimaud <agrimaud@iobeya.com>
2020-10-13 01:13:25 +0200

    and a on/off behavior

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,9 +1,22 @@
 chrome.browserAction.onClicked.addListener(placeholdify);
 
+var toggle = false;
+
 function placeholdify() {
-  chrome.tabs.executeScript({
-    file: "placeholdify.js",
-    allFrames: true,
-    frameId: 0,
-  });
+  toggle = !toggle;
+  if(toggle){
+    chrome.browserAction.setIcon({path: "./assets/on/icon128.png"});
+    chrome.tabs.executeScript({
+      file: "placeholdify.js",
+      allFrames: true,
+      frameId: 0,
+    })
+  } else {
+    chrome.browserAction.setIcon({path: "./assets/off/icon128.png"});
+    chrome.tabs.executeScript({
+      file: "placeholdify.js",
+      allFrames: true,
+      frameId: 0,
+    })
+  }
 }
